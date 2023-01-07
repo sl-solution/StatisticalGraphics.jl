5cf0b9a8407e28774278ced13aa705280d71c426
sl-solution <github.sl.solution@icloud.com>
2023-01-07 21:15:49 +1300

    bug fix - Pie chart

--- src/charts/pie.jl
+++ src/charts/pie.jl
@@ -207,7 +207,7 @@ function _check_and_normalize!(plt::Pie, all_args)
         _f = identity
     end
 
-    g_col = _extra_col_for_panel
+    g_col = copy(_extra_col_for_panel)
   
     # we need to refer to the names in bar_ds not ds - so index are not useful
     _extra_col_for_panel_names_ = names(ds, _extra_col_for_panel)
