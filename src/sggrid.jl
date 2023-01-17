244d95583651fc590792ce747fa571002e3f0fb3
sl-solution <github.sl.solution@icloud.com>
2023-01-17 23:50:25 +1300

    bug fix + enhancement

--- src/sggrid.jl
+++ src/sggrid.jl
@@ -3,7 +3,9 @@ mutable struct SGGrid <: SGPlots
 end
 SGGRID_DEFAULT = Dict{Symbol, Any}(:align => :none, # :all, :each, :none
     :columns => nothing,
-    :backcolor=>nothing
+    :backcolor=>nothing,
+    :center=>[false, false],
+    :bounds=>:full # :full/:flush see vega docs
 )
 
 
@@ -11,21 +13,29 @@ SGGRID_DEFAULT = Dict{Symbol, Any}(:align => :none, # :all, :each, :none
 function sggrid(sgp::Vector{T}; opts...) where T <: SGPlots
     optsd = val_opts(opts)
     global_opts = update_default_opts!(deepcopy(SGGRID_DEFAULT), optsd)
+    if global_opts[:center] isa Bool
+        global_opts[:center] = fill(global_opts[:center], 2)
+    end
 
     vspec = Dict{Symbol,Any}()
     vspec[:background] = something(global_opts[:backcolor], sgp[1].json_spec[:background])
     vspec[Symbol("\$schema")] = "https://vega.github.io/schema/vega/v5.json"
-    vspec[:layout] = Dict{Symbol, Any}(:align => global_opts[:align])
+    vspec[:layout] = Dict{Symbol, Any}(:align => global_opts[:align], 
+                                       :center => Dict{Symbol, Any}(:row=>global_opts[:center][1],
+                                       :column=>global_opts[:center][2]),
+                                       :bounds => global_opts[:bounds])
     if global_opts[:columns] !== nothing
         vspec[:layout][:columns] = global_opts[:columns]
     end
     vspec[:marks] = Dict{Symbol, Any}[]
 
-    mks = [:data, :signals, :scales, :legends, :axes, :marks, :layout]
+    mks = [:data, :signals, :scales, :legends, :axes, :marks, :layout, :config]
 
     for p in sgp
         spec = Dict{Symbol, Any}()
         spec[:type] = "group"
+        spec[:encode] = Dict{Symbol, Any}()
+        spec[:encode][:update] = Dict{Symbol, Any}()
         for mk in mks
             if haskey(p.json_spec, mk)
                 spec[mk] = p.json_spec[mk]
@@ -37,11 +47,14 @@ function sggrid(sgp::Vector{T}; opts...) where T <: SGPlots
         if haskey(p.json_spec, :width) # p must have :height too
             push!(spec[:signals], Dict{Symbol, Any}(:name=>:width, :value=>p.json_spec[:width]))
             push!(spec[:signals], Dict{Symbol, Any}(:name=>:height, :value=>p.json_spec[:height]))
-            spec[:encode] = Dict{Symbol, Any}()
-            spec[:encode][:update] = Dict{Symbol, Any}()
             spec[:encode][:update][:width] = Dict{Symbol, Any}(:value=>p.json_spec[:width])
             spec[:encode][:update][:height] = Dict{Symbol, Any}(:value=>p.json_spec[:height])
         end
+        if haskey(p.json_spec, :config) && haskey(p.json_spec[:config], :group)
+            # wallcolor is the only configuration we set - this may need revise in future
+            spec[:encode][:update][:fill] = Dict{Symbol, Any}(:value => p.json_spec[:config][:group][:fill]) 
+        end
+
         push!(vspec[:marks], spec)
     end
     SGGrid(vspec)
