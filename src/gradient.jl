8a2bfbd92010a46fda3b68b9ebec6d570820fdc9
sl-solution <github.sl.solution@icloud.com>
2023-03-06 21:43:53 +1300

    Update gradient.jl

--- src/gradient.jl
+++ src/gradient.jl
@@ -1,5 +1,5 @@
 """
-    gradient(colors...; direction = [0,1,0,0])
+    gradient([colors...]; direction = [0,1,0,0])
 
 A linear gradient interpolates colors along a line, from a starting point to an ending point. By default a linear gradient runs horizontally, from left to right. Use the direction to configure the gradient direction, e.g. [0,0,0,1] runs the gradient vertically. All coordinates are defined in a normalized [0, 1] coordinate space, relative to the bounding box of the item being colored.
 
